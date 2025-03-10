df2123b28e7a55f30bde41065a0af6f426627297
Yongshun Shreck Ye <ShreckYe@gmail.com>
2025-03-10 16:15:42 +0800

    Temp WIP

--- main/init/gradle/src/mill/main/gradle/Dep.java
+++
@@ -1,8 +0,0 @@
-package mill.main.gradle;
-
-import org.gradle.api.artifacts.Dependency;
-import org.gradle.api.artifacts.ExternalDependency;
-import org.gradle.api.artifacts.ProjectDependency;
-import org.gradle.util.internal.VersionNumber;
-
-import java.io.Serializable;
